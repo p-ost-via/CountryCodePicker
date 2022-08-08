1a7f186cb298187f146963c607e6a1215375a365
paulostasha <paul.ostasha@ridewithvia.com>
2022-08-08 14:30:44 +0300

    material dialog

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -284,7 +284,7 @@ class CountryCodePickerState extends State<CountryCodePicker> {
 
   void showCountryCodePickerDialog() {
     if (!UniversalPlatform.isAndroid && !UniversalPlatform.isIOS) {
-      showOverlappingDialog(
+      showMaterialDialog(
         barrierColor: widget.barrierColor ?? Colors.grey.withOpacity(0.5),
         // backgroundColor: widget.backgroundColor ?? Colors.transparent,
         context: context,
--- lib/overlapping_dialog_route.dart
+++ lib/overlapping_dialog_route.dart
@@ -58,6 +58,62 @@ class OverlappingDialogRoute<T> extends RawDialogRoute<T> {
   }
 }
 
+class MaterialDialogRoute<T> extends PageRoute<T> {
+  MaterialDialogRoute({
+    required this.pageBuilder,
+    bool barrierDismissible = true,
+    Color? barrierColor = const Color(0x80000000),
+    String? barrierLabel,
+    Duration transitionDuration = const Duration(milliseconds: 200),
+    RouteTransitionsBuilder? transitionBuilder,
+    RouteSettings? settings,
+  })  : assert(barrierDismissible != null),
+        _barrierDismissible = barrierDismissible,
+        _barrierLabel = barrierLabel,
+        _barrierColor = barrierColor,
+        _transitionDuration = transitionDuration,
+        super(settings: settings);
+
+  final RoutePageBuilder pageBuilder;
+
+  @override
+  bool get barrierDismissible => _barrierDismissible;
+  final bool _barrierDismissible;
+
+  @override
+  String? get barrierLabel => _barrierLabel;
+  final String? _barrierLabel;
+
+  @override
+  Color? get barrierColor => _barrierColor;
+  final Color? _barrierColor;
+
+  @override
+  Duration get transitionDuration => _transitionDuration;
+  final Duration _transitionDuration;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return Semantics(
+      scopesRoute: true,
+      explicitChildNodes: true,
+      child: pageBuilder(context, animation, secondaryAnimation),
+    );
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    final PageTransitionsTheme theme = Theme.of(context).pageTransitionsTheme;
+    return theme.buildTransitions<T>(
+        this, context, animation, secondaryAnimation, child);
+  }
+
+  @override
+  bool get maintainState => false;
+}
+
 Widget _buildMaterialDialogTransitions(
     BuildContext context,
     Animation<double> animation,
@@ -97,7 +153,7 @@ Future<T?> showOverlappingDialog<T>({
     ).context,
   );
 
-  return Navigator.of(context, rootNavigator: false)
+  return Navigator.of(context, rootNavigator: useRootNavigator)
       .push<T>(OverlappingDialogRoute<T>(
     context: context,
     builder: builder,
@@ -109,4 +165,32 @@ Future<T?> showOverlappingDialog<T>({
     themes: themes,
     anchorPoint: anchorPoint,
   ));
+}
+
+Future<T?> showMaterialDialog<T>({
+  required BuildContext context,
+  required WidgetBuilder builder,
+  bool barrierDismissible = true,
+  Color? barrierColor = Colors.black54,
+  String? barrierLabel,
+  bool useSafeArea = true,
+  bool useRootNavigator = true,
+  RouteSettings? routeSettings,
+  Offset? anchorPoint,
+}) {
+  assert(builder != null);
+  assert(barrierDismissible != null);
+  assert(useSafeArea != null);
+  assert(useRootNavigator != null);
+  assert(debugCheckHasMaterialLocalizations(context));
+
+  return Navigator.of(context, rootNavigator: useRootNavigator).push<T>(
+    MaterialDialogRoute<T>(
+      pageBuilder: (context, animation, secondaryAnimation) => builder(context),
+      barrierColor: barrierColor,
+      barrierDismissible: barrierDismissible,
+      barrierLabel: barrierLabel,
+      settings: routeSettings,
+    ),
+  );
 }
